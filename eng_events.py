908a3c8b3b12fa27dd324138e08266fe2cb7a23e
pchee <parkchunghee@gmail.com>
2023-06-22 10:44:19 +0900

    ver1

---
+++ eng_events.py
@@ -0,0 +1,36 @@
+
+def HomeBtnClicked(event_struct):
+    return
+
+
+def UpdateBtnClicked(event_struct):
+    return
+
+
+def ClearDtcBtnClicked(event_struct):
+    return
+
+
+def RebootBtnClicked(event_struct):
+    return
+
+
+def DltDownloadBtnClicked(event_struct):
+    return
+
+
+def EnParkModeBtnClicked(event_struct):
+    return
+
+
+def DisableParkModeBtnClicked(event_struct):
+    return
+
+
+def GsensorLogOnBtnClicked(event_struct):
+    return
+
+
+def GsensorLogOffBtnClicked(event_struct):
+    return
+
